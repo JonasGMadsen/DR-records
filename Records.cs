e3b8155449a86f077a9ece957c3b4facca9cc80b
Jonas Madsen <jonas6070@hotmail.com>
2023-11-13 09:59:24 +0100

    Made model class and repository

---
+++ Records.cs
@@ -0,0 +1,12 @@
+﻿namespace DR_records
+{
+    public class Records
+    {
+        public int id {  get; set; }
+        public string title {  get; set; }
+        public string artist { get; set; }
+        public int duration { get; set; }
+        public int publicationYear { get; set; }
+
+    }
+}
---
+++ RecordsRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace DR_records
+{
+    public class RecordsRepository
+    {
+        private List<Records> _records;
+        private int _nextId;
+        
+        public RecordsRepository()
+        {
+            _nextId = 1;
+            _records = new List<Records>()
+            {
+                new Records() {id = _nextId++, title = "Wish you were here", artist = "Pink Floyd", duration = 50, publicationYear = 1997 }
+            };
+        }
+
+        public List<Records> GetRecords()
+        {
+            return new List<Records>(_records);
+        }
+    }
+}
+
