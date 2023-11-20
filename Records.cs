d4187d275e00781c1af72650f95ba9ecf1eb08db
Jeppe <Olsen1906@gmail.com>
2023-11-20 11:42:10 +0100

    Removed if statement  for swagger in program.

--- Records.cs
+++ Records.cs
@@ -33,7 +33,7 @@
         {
             if (duration <= 0)
             {
-                throw new ArgumentOutOfRangeException("Duration must be greater than 0");
+                throw new Exception("Duration must be greater than 0");
             }
         }
 
@@ -41,7 +41,7 @@
         {
             if (publicationYear <= 1800)
             {
-                throw new ArgumentOutOfRangeException("Publication year must be greater than 1800");
+                throw new Exception("Publication year must be greater than 1800");
             }
         }
 
